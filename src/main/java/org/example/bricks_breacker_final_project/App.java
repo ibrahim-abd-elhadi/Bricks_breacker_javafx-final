e13a855edbbb6a795d8babf64912a5acfb2ee688
EssamShenhab <152318011+EssamShenhab@users.noreply.github.com>
2024-05-21 01:43:53 +0300

    Add files via upload

---
+++ src/main/java/org/example/bricks_breacker_final_project/App.java
@@ -0,0 +1,9 @@
+package org.example.bricks_breacker_final_project;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
